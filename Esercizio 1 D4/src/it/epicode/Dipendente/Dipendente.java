6bec66eb819aa3600665e6273761bc3ef5228deb
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-01-12 10:14:23 +0100

    Initial commit, not finished yet

---
+++ Esercizio 1 D4/src/it/epicode/Dipendente/Dipendente.java
@@ -0,0 +1,9 @@
+package it.epicode.Dipendente;
+
+public class Dipendente {
+  public static int stipendioBase = 1000;
+    private String matricola;
+   private int stipendio;
+   private int importoOrarioStraordinario;
+
+}
---
+++ Esercizio 1 D4/src/it/epicode/GestioneDipendenti/GestioneDipendenti.java
@@ -0,0 +1,4 @@
+package it.epicode.GestioneDipendenti;
+
+public class GestioneDipendenti {
+}
